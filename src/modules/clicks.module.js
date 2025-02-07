028a4171d1a11b10b0f036047227e5cea4ae6d66
Oleg Portnov <drose1@list.ru>
2025-02-07 19:51:40 +0400

    test click.module.js

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,5 +1,12 @@
 import {Module} from '../core/module'
 
 export class ClicksModule extends Module {
+    constructor(type, text){
+        this.type = type;
+        this.text = text;
+    }
 
+    onClick(){
+        
+    }
 }
